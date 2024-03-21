1f84536669fa53dd1f729c959ea70001329b19fc
Joshua Lau <dachshunduniverse@gmail.com>
2024-03-21 14:23:49 -0400

    Add mobile warning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -96,209 +96,216 @@
   <meta name="author" content="Joshua Lau" />
 </svelte:head>
 
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<a href="/">
-					<strong class="text-xl">HiraHero</strong>
-				</a>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<button
+<div class="block sm:hidden">
+    <h1 class="text-3xl text-center mt-8 p-4">
+      HiraHero is not supported on mobile devices. Please use a desktop browser.
+    </h1>
+</div>
+<div class="hidden sm:block">
+  <AppShell>
+    <svelte:fragment slot="header">
+      <!-- App Bar -->
+      <AppBar>
+        <svelte:fragment slot="lead">
+          <a href="/">
+            <strong class="text-xl">HiraHero</strong>
+          </a>
+        </svelte:fragment>
+        <svelte:fragment slot="trail">
+          <button
+            class="btn btn-sm variant-ghost-surface"
+            on:click={() => {
+              if (confirm("Are you sure you want to reset your progress?")) {
+                localStorage.clear();
+                location.reload();
+                cpm.reset();
+              }
+            }}
+          >
+            Reset
+          </button>
+          <button
           class="btn btn-sm variant-ghost-surface"
           on:click={() => {
-            if (confirm("Are you sure you want to reset your progress?")) {
-              localStorage.clear();
+            if (confirm("Are you sure you want to enable all characters?")) {
+              learned = HIRAGANA;
+              localStorage.setItem("learned", JSON.stringify(learned));
               location.reload();
-              cpm.reset();
             }
           }}
         >
-          Reset
+          Enable All
         </button>
-        <button
-        class="btn btn-sm variant-ghost-surface"
-        on:click={() => {
-          if (confirm("Are you sure you want to enable all characters?")) {
-            learned = HIRAGANA;
-            localStorage.setItem("learned", JSON.stringify(learned));
-            location.reload();
-          }
-        }}
-      >
-        Enable All
-      </button>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-  <div class="max-w-7xl mx-auto px-8">
-    {#if ready}
-    <div>
-      <h1 class="text-4xl text-center mt-8">Learn Japanese Characters</h1>
-    </div>
-    <div class="flex justify-center w-full mt-6">
-      {#each currentDisplay as character, index}
-        <div class="character {index === 3 && 'underline'}" 
-        style="opacity: {calculateOpacity(index)}">
-          {character}
-        </div>
-      {/each}
-    </div>
-    {/if}
-
-    <div>
-      <p class="text-center mt-8 space-x-8">
-        <span>
-          CPM: {currentCPM.toFixed(2)} 
-        </span>
-        <span>
-          Characters: {numInCycle} 
-        </span>
-      </p>
-    </div>
+        </svelte:fragment>
+      </AppBar>
+    </svelte:fragment>
+    <div class="max-w-7xl mx-auto px-8">
+      {#if ready}
+      <div>
+        <h1 class="text-4xl text-center mt-8">Learn Japanese Characters</h1>
+      </div>
+      <div class="flex justify-center w-full mt-6">
+        {#each currentDisplay as character, index}
+          <div class="character {index === 3 && 'underline'}" 
+          style="opacity: {calculateOpacity(index)}">
+            {character}
+          </div>
+        {/each}
+      </div>
+      {/if}
   
-    <img src={keyboardJp} alt="Japanese Keyboard" class="w-2/3 mx-auto mt-10" />
-
-    <div class="w-3/4 flex justify-center mx-auto gap-8">
-      <div class="w-64 mx-auto mt-6">
-        {#key refresher}
-          <p>CPM Threshold ({cpm.getCpmThreshold()})</p>
-        {/key}
-        <div class="input-group input-group-divider grid-cols-[1fr_auto]">
-          <input type="text" bind:value={threshInput} />
-          <button class="variant-filled-secondary" on:click={() => {
-            const conv = parseInt(threshInput);
-            if (isNaN(conv) || conv <= 0) {
+      <div>
+        <p class="text-center mt-8 space-x-8">
+          <span>
+            CPM: {currentCPM.toFixed(2)} 
+          </span>
+          <span>
+            Characters: {numInCycle} 
+          </span>
+        </p>
+      </div>
+    
+      <img src={keyboardJp} alt="Japanese Keyboard" class="w-2/3 mx-auto mt-10" />
+  
+      <div class="w-3/4 flex justify-center mx-auto gap-8">
+        <div class="w-44 mx-auto mt-6">
+          {#key refresher}
+            <p>CPM Threshold ({cpm.getCpmThreshold()})</p>
+          {/key}
+          <div class="input-group input-group-divider grid-cols-[1fr_auto]">
+            <input type="text" bind:value={threshInput} />
+            <button class="variant-filled-secondary" on:click={() => {
+              const conv = parseInt(threshInput);
+              if (isNaN(conv) || conv <= 0) {
+                const t = {
+                  message: `Invalid CPM threshold`,
+                  duration: 3000,
+                  background: 'variant-filled-error'
+                };
+                toast.trigger(t);
+                return;
+              }
+  
+              cpm.setCpmThreshold(conv);
+              refresher = !refresher;
+  
               const t = {
-                message: `Invalid CPM threshold`,
-                duration: 3000,
-                background: 'variant-filled-error'
+                message: `CPM threshold set to ${conv}`,
+                duration: 3000
               };
               toast.trigger(t);
-              return;
-            }
-
-            cpm.setCpmThreshold(conv);
-            refresher = !refresher;
-
-            const t = {
-              message: `CPM threshold set to ${conv}`,
-              duration: 3000
-            };
-            toast.trigger(t);
-          }}>
-            Enter
-          </button>
+            }}>
+              Enter
+            </button>
+          </div>
+        </div>
+        <div class="w-44 mx-auto mt-6">
+          {#key refresher}
+            <p>Min Window ({cpm.getMinWindow()})</p>
+          {/key}
+          <div class="input-group input-group-divider grid-cols-[1fr_auto]">
+            <input type="text" bind:value={minWidowInput} />
+            <button class="variant-filled-secondary" on:click={() => {
+              const conv = parseInt(minWidowInput);
+              if (isNaN(conv) || conv < 2) {
+                const t = {
+                  message: `Invalid min window size`,
+                  duration: 3000,
+                  background: 'variant-filled-error'
+                };
+                toast.trigger(t);
+                return;
+              }
+  
+              if (conv > cpm.getMaxWindow()) {
+                const t = {
+                  message: `Min window size must be less than or equal to max window size`,
+                  duration: 3000,
+                  background: 'variant-filled-error'
+                };
+                toast.trigger(t);
+                return;
+              }
+  
+              cpm.setMinWindow(conv);
+              refresher = !refresher;
+  
+              const t = {
+                message: `Min window size set to ${conv}`,
+                duration: 3000
+              };
+              toast.trigger(t);
+            }}>
+              Enter
+            </button>
+          </div>
+        </div>
+        <div class="w-44 mx-auto mt-6">
+          {#key refresher}
+            <p>Max Window ({cpm.getMaxWindow()})</p>
+          {/key}
+          <div class="input-group input-group-divider grid-cols-[1fr_auto]">
+            <input type="text" bind:value={maxWindowInput} />
+            <button class="variant-filled-secondary" on:click={() => {
+              const conv = parseInt(maxWindowInput);
+              if (isNaN(conv) || conv < 2) {
+                const t = {
+                  message: `Invalid max window size`,
+                  duration: 3000,
+                  background: 'variant-filled-error'
+                };
+                toast.trigger(t);
+                return;
+              } 
+  
+              if (conv < cpm.getMinWindow()) {
+                const t = {
+                  message: `Max window size must be greater than or equal to min window size`,
+                  duration: 3000,
+                  background: 'variant-filled-error'
+                };
+                toast.trigger(t);
+                return;
+              }
+  
+              cpm.setMaxWindow(conv);
+              refresher = !refresher;
+  
+              const t = {
+                message: `Max window size set to ${conv}`,
+                duration: 3000
+              };
+              toast.trigger(t);
+            }}>
+              Enter
+            </button>
+          </div>
         </div>
       </div>
-      <div class="w-64 mx-auto mt-6">
+  
+      <div class="mt-14 space-y-2 w-2/3 mx-auto">
+        <h2 class="text-center text-xl">
+          Gameplay
+        </h2>
         {#key refresher}
-          <p>Min Window ({cpm.getMinWindow()})</p>
+        <p class="text-center">
+          Type the character in the middle of the screen to progress to the next character.
+          Once you reach {cpm.getCpmThreshold()} characters-per-minute (CPM) 
+          with at least {cpm.getMinWindow()} typed characters 
+          (with a max window size of {cpm.getMaxWindow()} previous types 
+          and max difference of 5 seconds between each character typed), 
+          you will progress to the next character, and the CPM will reset.
+        </p>
         {/key}
-        <div class="input-group input-group-divider grid-cols-[1fr_auto]">
-          <input type="text" bind:value={minWidowInput} />
-          <button class="variant-filled-secondary" on:click={() => {
-            const conv = parseInt(minWidowInput);
-            if (isNaN(conv) || conv < 2) {
-              const t = {
-                message: `Invalid min window size`,
-                duration: 3000,
-                background: 'variant-filled-error'
-              };
-              toast.trigger(t);
-              return;
-            }
-
-            if (conv > cpm.getMaxWindow()) {
-              const t = {
-                message: `Min window size must be less than or equal to max window size`,
-                duration: 3000,
-                background: 'variant-filled-error'
-              };
-              toast.trigger(t);
-              return;
-            }
-
-            cpm.setMinWindow(conv);
-            refresher = !refresher;
-
-            const t = {
-              message: `Min window size set to ${conv}`,
-              duration: 3000
-            };
-            toast.trigger(t);
-          }}>
-            Enter
-          </button>
-        </div>
-      </div>
-      <div class="w-64 mx-auto mt-6">
-        {#key refresher}
-          <p>Max Window ({cpm.getMaxWindow()})</p>
-        {/key}
-        <div class="input-group input-group-divider grid-cols-[1fr_auto]">
-          <input type="text" bind:value={maxWindowInput} />
-          <button class="variant-filled-secondary" on:click={() => {
-            const conv = parseInt(maxWindowInput);
-            if (isNaN(conv) || conv < 2) {
-              const t = {
-                message: `Invalid max window size`,
-                duration: 3000,
-                background: 'variant-filled-error'
-              };
-              toast.trigger(t);
-              return;
-            } 
-
-            if (conv < cpm.getMinWindow()) {
-              const t = {
-                message: `Max window size must be greater than or equal to min window size`,
-                duration: 3000,
-                background: 'variant-filled-error'
-              };
-              toast.trigger(t);
-              return;
-            }
-
-            cpm.setMaxWindow(conv);
-            refresher = !refresher;
-
-            const t = {
-              message: `Max window size set to ${conv}`,
-              duration: 3000
-            };
-            toast.trigger(t);
-          }}>
-            Enter
-          </button>
-        </div>
+        <p class="text-center">
+          Progress is saved locally, so you can continue where you left off.
+          You can reset your progress by clicking the "Reset" button in 
+          the top right corner.
+        </p>
       </div>
     </div>
-
-    <div class="mt-14 space-y-2 w-2/3 mx-auto">
-      <h2 class="text-center text-xl">
-        Gameplay
-      </h2>
-      {#key refresher}
-      <p class="text-center">
-        Type the character in the middle of the screen to progress to the next character.
-        Once you reach {cpm.getCpmThreshold()} characters-per-minute (CPM) 
-        with at least {cpm.getMinWindow()} typed characters 
-        (with a max window size of {cpm.getMaxWindow()} previous types 
-        and max difference of 5 seconds between each character typed), 
-        you will progress to the next character, and the CPM will reset.
-      </p>
-      {/key}
-      <p class="text-center">
-        Progress is saved locally, so you can continue where you left off.
-        You can reset your progress by clicking the "Reset" button in 
-        the top right corner.
-      </p>
-    </div>
-  </div>
-</AppShell>
+  </AppShell>
+</div>
 
 
 <style lang="postcss">
