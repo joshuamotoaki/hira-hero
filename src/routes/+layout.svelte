6bed30957d3a0296436bb06e1ede95ed2f662275
Joshua Lau <dachshunduniverse@gmail.com>
2024-03-20 22:05:55 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<a href="/">
+					<strong class="text-xl">SwiftGlyphs</strong>
+				</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+				class="btn btn-sm variant-ghost-surface"
+				href="/about"
+				>
+					About
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
